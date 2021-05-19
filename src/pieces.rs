f35b822044d0bfcf91f5732d86d5314971ee34d5
User5100 <maot.tm@gmail.com>
2021-05-19 10:09:13 +0100

    validate available squares

--- src/pieces.rs
+++ src/pieces.rs
@@ -411,6 +411,67 @@ fn move_pieces(time: Res<Time>, mut query: Query<(&mut Transform, &Piece)>) {
   }
 }
 
+fn color_of_square(pos: (u8, u8), pieces: &Vec<Piece>) -> Option<PieceColor> {
+  for piece in pieces {
+    if piece.x == pos.0 && piece.y == pos.1 {
+      return Some(piece.color);
+    }
+  }
+  None
+}
+
+fn is_path_empty(begin: (u8, u8), end: (u8, u8), pieces: &Vec<Piece>) -> bool {
+  // same column
+  if begin.0 == end.0 {
+    for piece in pieces {
+      if piece.x == begin.0
+        && ((piece.y > begin.1 && piece.y < end.1) || (piece.y > end.1 && piece.y < begin.1))
+      {
+        return false;
+      }
+    }
+  }
+
+  //same row
+  if begin.1 == end.1 {
+    for piece in pieces {
+      if piece.y == end.0
+        && ((piece.x > begin.0 && piece.x < end.0) || (piece.x > end.0 && piece.x < begin.0))
+      {
+        return true;
+      }
+    }
+  }
+
+  // Diagonals
+  let x_diff = (begin.0 as i8 - end.0 as i8).abs();
+  let y_diff = (begin.1 as i8 - end.1 as i8).abs();
+  if x_diff == y_diff {
+    for i in 1..x_diff {
+      let pos = if begin.0 < end.0 && begin.1 < end.1 {
+        // left bottom - right top
+        (begin.0 + i as u8, begin.1 + i as u8)
+      } else if begin.0 < end.0 && begin.1 > end.1 {
+        // left top - right bottom
+        (begin.0 + i as u8, begin.1 - i as u8)
+      } else if begin.0 > end.0 && begin.1 < end.1 {
+        // right bottom - left top
+        (begin.0 - i as u8, begin.1 + i as u8)
+      } else {
+        // begin.0 > end.0 && begin.1 > end.1
+        // right top - left bottom
+        (begin.0 - i as u8, begin.1 - i as u8)
+      };
+
+      if color_of_square(pos, pieces).is_some() {
+        return false;
+      }
+    }
+  }
+
+  true
+}
+
 pub struct PiecesPlugin;
 
 impl Plugin for PiecesPlugin {
